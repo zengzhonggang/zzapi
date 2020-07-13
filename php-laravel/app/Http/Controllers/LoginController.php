433efcb2adb98d8ffc409668a8e33f9596d6f988
zengzhonggang <zengzhonggang@kf5.com>
2020-07-13 08:15:06 +0800

    init

---
+++ php-laravel/app/Http/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zeroking
+ * Date: 2020-07-13
+ * Time: 01:17
+ */
+
+namespace App\Http\Controllers;
+
+
+class LoginController extends Controller
+{
+    public function index(){
+        return ['status'=>'ok'];
+    }
+
+}
